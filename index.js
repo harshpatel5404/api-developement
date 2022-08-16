31d5262fa6154f5cd99953ed46120d20031049c1
Harshpatel5404 <harshpatel5404@gmail.com>
2022-08-16 12:08:49 +0530

    testing added...

--- index.js
+++ index.js
@@ -1,126 +1,111 @@
-const express = require('express')
-const app = express()
-const dotenv = require("dotenv")
-const cors = require("cors")
-const userRouter = require("./routers/user")
-const authRouter = require("./routers/auth")
-const { default: mongoose } = require('mongoose')
-// const image = require("./modules/Image")
-const myFile = require("./modules/Image")
-const multer = require("multer")
-const fs = require("fs")
+// const express = require('express')
+// const app = express()
+// const dotenv = require("dotenv")
+// const cors = require("cors")
+// const userRouter = require("./routers/user")
+// const authRouter = require("./routers/auth")
+// const { default: mongoose } = require('mongoose')
+// // const image = require("./modules/Image")
+// const myFile = require("./modules/Image")
+// const multer = require("multer")
+// const fs = require("fs")
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
-dotenv.config()
-mongoose.connect("mongodb+srv://harsh5404:har5404@cluster0.kmphfal.mongodb.net/?retryWrites=true&w=majority",
-// mongoose.connect(process.env.MONGO_URL,
+// dotenv.config()
+// mongoose.connect("mongodb+srv://harsh5404:har5404@cluster0.kmphfal.mongodb.net/?retryWrites=true&w=majority",
+// // mongoose.connect(process.env.MONGO_URL,
 
-    {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+//     {
+//         keepAlive: true,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
 
-    }
-).then(() => console.log("Db Connected!")).catch(() => console.log("Error while db connection"));
-app.use(cors());
-app.use(express.json());
-app.get("/", (req,res)=> res.send("hellos woe!"));
-app.use("/api/auth", authRouter);
-
-
-
-
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads')
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.fieldname + '-' + Date.now())
 //     }
-// })
+// ).then(() => console.log("Db Connected!")).catch(() => console.log("Error while db connection"));
+// app.use(cors());
+// app.use(express.json());
+// app.get("/", (req,res)=> res.send("hellos woe!"));
+// app.use("/api/auth", authRouter);
 
-// var upload = multer({ storage: storage })
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "public");
+//     },
+//     filename: (req, file, cb) => {
+//         const ext = file.mimetype.split("/")[1];
+//         cb(null, `files/admins-${file.fieldname}-${Date.now()}.${ext}`);
+//     },
+// });
+
+// const upload = multer({
+//     storage: multerStorage,
+//     // fileFilter: multerFilter,
+// });
+
+// app.post("/api/uploadFile", upload.single("myFile"), async (req, res) => {
+//     try {
+//         const newFile = await myFile.create({
+//             name: req.file.filename,
+//         });
+//        res.status(200).json({
+//             status: "success",
+//             message: "File created successfully!!",
+//             save: newFile
+//         });
+//     } catch (error) {
+//         res.json({
+//             error,
+//         });
+//     }
+//     console.log(req.file);
+// });
 
 
-// app.get("/hi", (req, res) => {
-//     res.render("index");
-// })
+// app.get("/api/getFiles", async (req, res) => {
+//     try {
+//         const files = await myFile.find();
+//         res.status(200).json({
+//             status: "success",
+//             files,
+//         });
+//     } catch (error) {
+//         res.json({
+//             status: "Fail",
+//             error,
+//         });
+//     }
+// });
+ 
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public");
-    },
-    filename: (req, file, cb) => {
-        const ext = file.mimetype.split("/")[1];
-        cb(null, `files/admins-${file.fieldname}-${Date.now()}.${ext}`);
-    },
+
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+
+// INIT
+const PORT = process.env.PORT || 3000;
+const app = express();
+const DB =
+  "mongodb+srv://harshvora:harshpatel98@cluster0.wkulyrj.mongodb.net/?retryWrites=true&w=majority";
+  
+// middleware
+app.use(express.json());
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+
+app.get("/", (req,res) => res.send("testing...."));
+
+app.listen(PORT, () => {
+  console.log(`connected at port ${PORT}`);
 });
-
-const upload = multer({
-    storage: multerStorage,
-    // fileFilter: multerFilter,
-});
-
-app.post("/api/uploadFile", upload.single("myFile"), async (req, res) => {
-    try {
-        const newFile = await myFile.create({
-            name: req.file.filename,
-        });
-
-        // let myfile = new myFile({
-        //   name :  req.file.filename,
-        // }); 
-        // let savedFile = await myfile.save();
-
-
-        res.status(200).json({
-            status: "success",
-            message: "File created successfully!!",
-            save: newFile
-        });
-    } catch (error) {
-        res.json({
-            error,
-        });
-    }
-    console.log(req.file);
-});
-
-
-app.get("/api/getFiles", async (req, res) => {
-    try {
-        const files = await myFile.find();
-        res.status(200).json({
-            status: "success",
-            files,
-        });
-    } catch (error) {
-        res.json({
-            status: "Fail",
-            error,
-        });
-    }
-});
-
-// app.post("/uploadphoto", upload.single('myImage'), (req, res) => {
-//     var img = fs.readFileSync(req.file.path);
-//     var encode_img = img.toString('base64');
-//     var final_img = {
-//         contentType: req.file.mimetype,
-//         image: Buffer(encode_img, 'base64')
-//     };
-//     image.create(final_img, function (err, result) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(result.img.Buffer);
-//             console.log("Saved To database");
-//             res.contentType(final_img.contentType);
-//             res.send(final_img.image);
-//         }
-//     })
-// })
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
